5f9d080404183ca7ccadb6278a3d611d26f0e892
unknown <shrikar.tare@gmail.com>
2024-04-18 14:17:25 +0530

    feat: Traffic park integrations

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,70 @@
 import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import HomePageView from "./views/HomePageView";
-import AboutUsView from "./views/AboutUsView";
-import ContactUsView from "./views/ContactUsView";
-import GalleryView from "./views/GalleryView";
-import QuizView from "./views/QuizView";
-import TestimonialView from "./views/TestimonialView";
+
+const HomepageView = React.lazy(() => import("./views/HomePageView"));
+
+const AboutUsView = React.lazy(() => import("./views/AboutUsView"));
+
+const ContactUsView = React.lazy(() => import("./views/ContactUsView"));
+
+const GalleryView = React.lazy(() => import("./views/GalleryView"));
+
+const TestimonialView = React.lazy(() => import("./views/TestimonialView"));
+
+const QuizView = React.lazy(() => import("./views/QuizView"));
 
 const App = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<HomePageView />} />
-        <Route path="/aboutus" element={<AboutUsView />} />
-        <Route path="/gallery" element={<GalleryView />} />
-        <Route path="/contactus" element={<ContactUsView />} />
-        <Route path="/quiz" element={<QuizView />} />
-        <Route path="/testimonials" element={<TestimonialView />} />
+        <Route
+          path="/"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <HomepageView />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/aboutus"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <AboutUsView />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/gallery"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <GalleryView />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/contactus"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <ContactUsView />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/quiz"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <QuizView />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/testimonials"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <TestimonialView />
+            </React.Suspense>
+          }
+        />
       </Routes>
     </HashRouter>
   );
